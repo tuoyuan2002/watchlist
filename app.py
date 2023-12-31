f2d884b2604ded1b14a22ed23bb5533b747fcd55
Tuoyuan <tuoyuan2002@163.com>
2023-12-31 18:42:38 +0800

    Add static files

--- app.py
+++ app.py
@@ -1,20 +1,52 @@
-from flask import Flask, url_for
-from markupsafe import escape
+from flask import Flask # 导入 Flask 类：创建程序对象 app
+from flask import url_for # 导入 url_for() 函数：生成 URL
+from flask import render_template # 导入 render_template() 函数：渲染模板
+from markupsafe import escape # 导入 escape() 函数：对 HTML 标签进行转义
+from faker import Faker # 导入 Faker 类：生成假数据
+import random # 导入 random 模块：生成随机数
 
+# 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app
 app = Flask(__name__)
 
+# 数据部分
+name = 'Tuoyuan'
+
+# 创建 Faker 对象，用于生成假数据
+fake = Faker()
+
+# 生成电影列表
+movies_list = []
+for _ in range(20):
+    movie = {
+        # title 属性使用 fake.sentence() 方法生成假数据, 参数 nb_words=3 表示生成三句假数据,rstrip('.') 表示去掉句号
+        "title": fake.sentence(nb_words=3).rstrip('.'),
+        "year": random.randint(1950, 2023)  # 假设电影年份在1950到2023之间
+    }
+    movies_list.append(movie)
+
+# 路由部分
 @app.route('/')
+def index():
+    # 渲染模板并传递参数：name 和 movies
+    return render_template('index.html', name=name, movies=movies_list)
+
+@app.route('/1')
 def hello():
+    # 返回一个字符串，包含HTML的h1标签和img标签
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
 @app.route('/index')
 @app.route('/home')
 def hello2():
+    # 返回欢迎信息
     return 'Welcome to My Watchlist!'
 
 @app.route('/user/<name>')
 def user_page(name):
-    return f'User: {escape(name)}'
+    # 将name进行转义，避免特殊字符引起的错误
+    name = escape(name)
+    # 返回格式化字符串，将转义后的name插入到字符串中
+    return f'User: {name}'
 
 @app.route('/test')
 def test_url_for():
