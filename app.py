08de10314a353b510d3ce09c19d06937a01c3ce7
Tuoyuan <tuoyuan2002@163.com>
2024-01-01 14:16:26 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ from flask_login import current_user # 导入 current_user 函数：获取当前
 
 # 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
 
 # 配置部分
 prefix = 'sqlite:///' # 数据库前缀
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')# 配置数据库文件路径
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 # 数据库实例
